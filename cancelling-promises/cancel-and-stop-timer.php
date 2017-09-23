446bfa137e0c01876087b82bb9d1fcbdb9d8acb0
Zhuk Sergey <seregazhuk88@gmail.com>
2017-09-23 15:40:23 +0300

    promise timers

---
+++ cancelling-promises/cancel-and-stop-timer.php
@@ -0,0 +1,26 @@
+<?php
+
+require '../vendor/autoload.php';
+
+use React\EventLoop\Timer\TimerInterface;
+use function \React\Promise\Timer\timeout;
+
+$loop = React\EventLoop\Factory::create();
+
+$resolve = function(callable $resolve, callable $reject) use ($loop, &$timer) {
+    $timer = $loop->addTimer(5, function() use ($resolve) {
+        echo "resolved\n";
+    });
+};
+
+$cancel = function(callable $resolve, callable $reject) use (&$timer) {
+    echo "cancelled\n";
+    /** @var TimerInterface $timer */
+    $timer->cancel();
+};
+
+$promise = new React\Promise\Promise($resolve, $cancel);
+
+timeout($promise, 2, $loop);
+
+$loop->run();
