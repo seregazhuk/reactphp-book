ab0f5071c8a1df5ad79c370a028514d139a9d9a4
Zhuk Sergey <seregazhuk88@gmail.com>
2017-09-23 15:23:33 +0300

    video streaming examples

---
+++ video-streaming/streaming-refactoring.php
@@ -0,0 +1,80 @@
+<?php
+
+require '../vendor/autoload.php';
+
+use React\Http\Server;
+use React\Http\Response;
+use React\EventLoop\Factory;
+use React\EventLoop\LoopInterface;
+use React\Stream\ReadableResourceStream;
+use Psr\Http\Message\ServerRequestInterface;
+
+class VideoStreaming
+{
+
+    /**
+     * @var LoopInterface
+     */
+    protected $eventLoop;
+
+    /**
+     * @param LoopInterface $eventLoop
+     */
+    public function __construct(LoopInterface $eventLoop)
+    {
+        $this->eventLoop = $eventLoop;
+    }
+
+    /**
+     * @param ServerRequestInterface $request
+     * @return Response
+     */
+    function __invoke(ServerRequestInterface $request)
+    {
+        $file = $this->getFilePath($request);
+        if (empty($file)) {
+            return new Response(200, ['Content-Type' => 'text/plain'], 'Video streaming server');
+        }
+
+        return $this->makeResponseFromFile($file);
+    }
+
+    /**
+     * @param string $filePath
+     * @return Response
+     */
+    protected function makeResponseFromFile($filePath)
+    {
+        if (!file_exists($filePath)) {
+            return new Response(404, ['Content-Type' => 'text/plain'], "Video $filePath doesn't exist on server.");
+        }
+
+        $stream = new ReadableResourceStream(fopen($filePath, 'r'), $this->eventLoop);
+
+        return new Response(200, ['Content-Type' => mime_content_type($filePath)], $stream);
+    }
+
+    /**
+     * @param ServerRequestInterface $request
+     * @return string
+     */
+    protected function getFilePath(ServerRequestInterface $request)
+    {
+        $file = $request->getQueryParams()['file'] ?? '';
+
+        if (empty($file)) return '';
+
+        return __DIR__ . DIRECTORY_SEPARATOR . 'media' . DIRECTORY_SEPARATOR . basename($file);
+    }
+}
+
+$loop = Factory::create();
+
+$videoStreaming = new VideoStreaming($loop);
+$server = new Server($videoStreaming);
+
+$socket = new \React\Socket\Server('127.0.0.1:8000', $loop);
+$server->listen($socket);
+
+echo 'Listening on ' . str_replace('tcp:', 'http:', $socket->getAddress()) . "\n";
+$loop->run();
