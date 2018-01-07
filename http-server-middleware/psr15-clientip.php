db4fcc8accef5a4b4617dcbde6788e54d5b33ef2
Zhuk Sergey <seregazhuk88@gmail.com>
2018-01-07 22:20:49 +0300

    psr middleware examples fix

--- http-server-middleware/psr15-clientip.php
+++ http-server-middleware/psr15-clientip.php
@@ -3,23 +3,26 @@
 require '../vendor/autoload.php';
 
 use FriendsOfReact\Http\Middleware\Psr15Adapter\PSR15Middleware;
+use Middlewares\Redirect;
 use React\EventLoop\Factory;
-use React\Http\Server;$loop = Factory::create();
+use React\Http\Server;
+
+$loop = Factory::create();
+
 $server = new Server([
+    new PSR15Middleware($loop, \Middlewares\ClientIp::class),
     new PSR15Middleware(
         $loop,
-        \Middlewares\ClientIp::class
-    ),
-    new PSR15Middleware(
-        $loop,
-        \Middlewares\ResponseTime::class
+        \Middlewares\Redirect::class,
+        [
+            ['/old-url' => '/new-url']
+        ]
     ),
     function(\Psr\Http\Message\RequestInterface $request){
         echo 'IP: ' . $request->getAttribute('client-ip');
         return new \React\Http\Response(200);
     }
 ]);
-
 
 $socket = new \React\Socket\Server('127.0.0.1:8000', $loop);
 $server->listen($socket);
--- http-server-middleware/psr15-group.php
+++ http-server-middleware/psr15-group.php
@@ -4,11 +4,18 @@ require '../vendor/autoload.php';
 
 use FriendsOfReact\Http\Middleware\Psr15Adapter\GroupedPSR15Middleware;
 use React\EventLoop\Factory;
-use React\Http\Server;$loop = Factory::create();
+use React\Http\Server;
+
+$loop = Factory::create();
+
 $server = new Server([
     (new GroupedPSR15Middleware($loop))
         ->withMiddleware(\Middlewares\ClientIp::class)
-        ->withMiddleware(\Middlewares\ResponseTime::class),
+        ->withMiddleware(
+            \Middlewares\Redirect::class,
+            [
+                ['/old-url' => '/new-url']
+            ]),
     function(\Psr\Http\Message\RequestInterface $request){
         echo 'IP: ' . $request->getAttribute('client-ip');
         return new \React\Http\Response(200);
