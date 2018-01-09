1dfa0efe612c4134638fcefdf461385ae76aee85
Zhuk Sergey <seregazhuk88@gmail.com>
2018-01-09 09:42:09 +0300

    http server error example

---
+++ video-streaming/server-with-error.php
@@ -0,0 +1,27 @@
+<?php
+
+require '../vendor/autoload.php';
+
+use React\Http\Server;
+use React\Http\Response;
+use React\EventLoop\Factory;
+use Psr\Http\Message\ServerRequestInterface;
+use React\Promise\Promise;
+
+$loop = Factory::create();
+
+$server = new Server(function (ServerRequestInterface $request) {
+    $request->wrongMethod();
+    return new Response(200);
+});
+
+$server->on('error', function (Exception $e) {
+    echo 'Error: ' . $e->getMessage() . PHP_EOL;
+});
+
+$socket = new \React\Socket\Server('127.0.0.1:8000', $loop);
+$server->listen($socket);
+
+echo 'Listening on ' . str_replace('tcp:', 'http:', $socket->getAddress()) . PHP_EOL;
+
+$loop->run();
