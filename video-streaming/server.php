68adeb32877e073a9424da136b596c3ae2cc1e09
Zhuk Sergey <seregazhuk88@gmail.com>
2018-02-25 12:39:44 +0300

    add webscraping example

--- video-streaming/server.php
+++ video-streaming/server.php
@@ -1,7 +1,7 @@
 <?php
 
 
-require '../vendor/autoload.php';
+require 'vendor/autoload.php';
 
 use React\Http\Server;
 use React\Http\Response;
@@ -15,18 +15,16 @@ $server = new Server(function (ServerRequestInterface $request) use ($loop) {
     $file = $params['video'] ?? '';
 
     if (empty($file)) {
-        return new Response(200, ['Content-Type' => 'text/plain'], 'Video streaming server');
+        return new Response(200, ['Content-Type' => 'text/plain'], 'Streaming server');
     }
 
     $filePath = __DIR__ . DIRECTORY_SEPARATOR . 'media' . DIRECTORY_SEPARATOR . $file;
-    @$fileStream = fopen($filePath, 'r');
-    if (!$fileStream) {
-        return new Response(404, ['Content-Type' => 'text/plain'], "Video $file doesn't exist on server.");
-    }
+    $filesystem = \React\Filesystem\Filesystem::create($loop);
+    $filesystem->file('test.txt')->open('r')->then(function(\React\Stream\ReadableStream $stream){
 
-    $video = new \React\Stream\ReadableResourceStream($fileStream, $loop);
+    });
 
-    return new Response(200, ['Content-Type' => mime_content_type($filePath)], $video);
+    return new Response(200, ['Content-Type' => mime_content_type($filePath)], $streamSomeHow);
 });
 
 $socket = new \React\Socket\Server('127.0.0.1:8000', $loop);
