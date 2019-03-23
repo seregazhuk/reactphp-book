aaea57b9ecd2f23cbfca4c77ae929b2eecc804fc
Zhuk Sergey <seregazhuk88@gmail.com>
2019-03-23 21:49:51 +0300

    Restful API: check not allowed method

--- restulf-api-with-mysql/src/Router.php
+++ restulf-api-with-mysql/src/Router.php
@@ -26,6 +26,8 @@ final class Router
         switch ($routeInfo[0]) {
             case Dispatcher::NOT_FOUND:
                 return new Response(404, ['Content-Type' => 'text/plain'],  'Not found');
+            case Dispatcher::METHOD_NOT_ALLOWED:
+                return new Response(405, ['Content-Type' => 'text/plain'],  'Method not allowed');
             case Dispatcher::FOUND:
                 $params = $routeInfo[2] ?? [];
                 return $routeInfo[1]($request, ... array_values($params));
